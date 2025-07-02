9f8c6e18c91fa57e5a4fb4ddefd07c911a238f57
tusharharyana <123148711+tusharharyana@users.noreply.github.com>
2025-07-02 08:06:46 +0530

    Time out window testing 90sec

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("Forget to add endpoint in github webhook workflow!")
 print("Changes added to test pull request workflow.")
 print("Testing UI changes in push workflow!")
+print("Testing 90 seconds time window for UI changes in push workflow!")
